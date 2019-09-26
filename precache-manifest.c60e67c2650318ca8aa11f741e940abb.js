139522e427b3bb4c6e662042a0a7275fe918417e
SchecterZa <phuengton.ch.57@ubu.ac.th>
2019-09-26 16:06:09 +0700

    Updates

---
+++ precache-manifest.c60e67c2650318ca8aa11f741e940abb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02a46fc511866dc2d96eec39fe2161fb",
+    "url": "/indexof/index.html"
+  },
+  {
+    "revision": "61e1baf88e689d40293e",
+    "url": "/indexof/static/css/main.1c81de07.chunk.css"
+  },
+  {
+    "revision": "589455c448fd50d01ba5",
+    "url": "/indexof/static/js/2.359e71fd.chunk.js"
+  },
+  {
+    "revision": "61e1baf88e689d40293e",
+    "url": "/indexof/static/js/main.84b20bec.chunk.js"
+  },
+  {
+    "revision": "ae270fbd249a58b2f1c7",
+    "url": "/indexof/static/js/runtime-main.2e4de217.js"
+  }
+]);
